c37c2612dd297326dbcbc60e97aeedddf4782c1c
Van Paul Dayag <dayag.vanpaul@gmail.com>
2023-01-13 03:32:31 +0800

    p10k: add installation of p10k

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/zsh
 
+#install oh my zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# download powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "Downloaded powerlevel10k"
 # force copy .config folder to Home directory as symbolink link
 cp -rsf ~/.dotfiles/.config ~/
+echo ".configs copied"
 
 # zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
