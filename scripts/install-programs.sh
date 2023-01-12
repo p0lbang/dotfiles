29abed9b57e5c5e33430ea390c74e5d40a401422
Van Paul Dayag <dayag.vanpaul@gmail.com>
2023-01-13 04:57:44 +0800

    feat: install needed programs to run scripts

--- scripts/install-programs.sh
+++ scripts/install-programs.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-sudo pacman -S alacritty unzip
+sudo pacman -S --needed base-devel alacritty unzip git rustup
+rustup toolchain install stable
+
+# install paru
+git clone https://aur.archlinux.org/paru.git /tmp/paru
+cd /tmp/paru
+makepkg -si
